b780a7a0d6afcceb016ef356e9dc02a048dbe8fa
Juani Testa <Juani.testa@hotmail.com>
2022-07-14 20:17:29 -0300

    Muestra del error y entrega

--- archivosjs/menu.js
+++ archivosjs/menu.js
@@ -13,7 +13,13 @@ const burgerGeneradas = [];
 
 const ObtenerBurgers = async () => {
   console.log("aca");
-  const resp = await fetch("../public/data.json");
+  const resp = await fetch("../public/data.json", {
+    mode: "no-cors",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
   const data = await resp.json();
   data.forEach((el) => {
     burgerGeneradas.push(
@@ -820,54 +826,3 @@ class BurgerAñadida {
   }
 }
 ObtenerBurgers();
-/* fetch(url, config); */
-
-/* fetch('https://jsonplaceholder.typicode.com/posts')
-.then(response => response.json())
-.then(res => {
-  console.log(res[0].title);
-  console.log(res[0].body);
-}); */
-
-/* const lista = document.getElementById("listado");
-fetch('https://jsonplaceholder.typicode.com/posts')
-.then(response => response.json())
-.then(res => {
-  res.forEach(post => {
-    const li = document.createElement("li");
-    li.innerHTML = `
-      <h2>${post.title}</h2>
-      <p>${post.body}</p>
-    `;
-    lista.append(li);
-  });
-}); */
-
-/* fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
-    title: "Mi post",
-    body: "Estamos conectando con una api",
-    userId: 1
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-.then(response => response.json())
-.then(response => console.log(response)) */
-
-/* fetch("./data.json")
-  .then((res) => res.json())
-  .then((data) => {
-    data.forEach((producto) => {
-      const li = document.createElement("li");
-      li.innerHTML = `
-                <h4>${producto.nombre}</h4>
-                <p>${producto.precio}</p>
-                <p>Código: ${producto.id}</p>
-                <hr/>
-            `;
-      lista.append(li);
-    });
-  }); */
